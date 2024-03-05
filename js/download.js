d79ae5e2af38d345f85a4ea6568dfc3590599edc
Enzo Arce Valdivia <enzoarce@hotmail.com>
2024-03-05 18:10:19 -0500

    ajuste

--- js/download.js
+++ js/download.js
@@ -2,39 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const openModalButtons = document.querySelectorAll(".open-modal");
     const closeModalButton = document.getElementById("closeModal");
     const pdfModal = document.getElementById("pdfModal");
-    const pdfContainer = document.getElementById("pdfContainer");
+    const pdfViewer = document.getElementById("pdfViewer");
 
     openModalButtons.forEach(function(button) {
         button.addEventListener("click", function() {
+            // Obtener la ruta del documento asociada al botón actual
             const pdfSrc = button.getAttribute("data-src");
-            loadPDF(pdfSrc);
+            // Cambiar la URL del visor de PDF con la ruta del documento
+            pdfViewer.src = pdfSrc;
             pdfModal.classList.remove("modal--hidden");
         });
     });
 
     closeModalButton.addEventListener("click", function() {
         pdfModal.classList.add("modal--hidden");
-        pdfContainer.innerHTML = ""; // Limpiar el contenido del contenedor de PDF al cerrar el modal
+        // Detener la carga del PDF al cerrar el modal
+        pdfViewer.src = "";
     });
-
-    function loadPDF(pdfSrc) {
-        pdfContainer.innerHTML = ""; // Limpiar el contenido del contenedor de PDF antes de cargar un nuevo documento
-        PDFJS.getDocument(pdfSrc).promise.then(function(pdf) {
-            pdf.getPage(1).then(function(page) {
-                const scale = 1.5;
-                const viewport = page.getViewport({ scale: scale });
-                const canvas = document.createElement("canvas");
-                const context = canvas.getContext("2d");
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-                const renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-                page.render(renderContext).promise.then(function() {
-                    pdfContainer.appendChild(canvas);
-                });
-            });
-        });
-    }
 });
