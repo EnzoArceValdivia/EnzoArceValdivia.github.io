fe7257fb022ddaca550a242e94fd31a97f87cf77
Enzo Arce Valdivia <enzoarce@hotmail.com>
2024-03-08 18:43:16 -0500

    ajuste2

--- js/verWord_Excel.js
+++ js/verWord_Excel.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const botonesVisualizarDoc = document.querySelectorAll(".open-word, .open-excel");
 
-    // Función para abrir el documento de Word o Excel en una nueva pestaña
+    // Función para abrir el documento de Word o Excel en Google Docs Viewer
     function abrirDocumento(docSrc) {
-        window.open(docSrc, "_blank");
+        // Generar la URL del documento en Google Docs Viewer
+        var viewerUrl = "https://docs.google.com/viewer?url=" + encodeURIComponent(docSrc);
+        // Abrir el documento en una nueva pestaña
+        window.open(viewerUrl, "_blank");
     }
 
     // Agregar eventos a los botones "Visualizar Word" y "Visualizar Excel"
@@ -11,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         boton.addEventListener("click", function() {
             // Obtener la ruta del documento asignada al botón actual
             var docSrc = this.getAttribute('data-src');
-            // Abrir el documento de Word o Excel en una nueva pestaña
+            // Abrir el documento de Word o Excel en Google Docs Viewer
             abrirDocumento(docSrc);
         });
     });
