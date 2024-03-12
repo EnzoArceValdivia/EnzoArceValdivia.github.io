07fc3b854be1649aa7069fce9c19ff133563c352
Enzo Arce Valdivia <enzoarce@hotmail.com>
2024-03-12 15:27:12 -0500

    otra prueba

--- js/verPDF.js
+++ js/verPDF.js
@@ -1,18 +1,35 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const botonesVisualizarPDF = document.querySelectorAll(".open-pdf");
+
+//     // Función para redirigir a la página del visor de PDF con el PDF especificado
+//     function redirigirAVisorPDF(pdfSrc) {
+//         window.open("visorpdf.html?pdf=" + encodeURIComponent(pdfSrc), "_blank");
+//     }
+
+//     // Agregar eventos a los botones "Visualizar PDF"
+//     botonesVisualizarPDF.forEach(function(boton) {
+//         boton.addEventListener("click", function() {
+//             // Obtener la ruta del PDF asignada al botón actual
+//             var pdfSrc = this.getAttribute('data-src');
+//             // Redirigir a la página del visor de PDF con el PDF especificado
+//             redirigirAVisorPDF(pdfSrc);
+//         });
+//     });
+// });
+
+
 document.addEventListener("DOMContentLoaded", function() {
     const botonesVisualizarPDF = document.querySelectorAll(".open-pdf");
-
-    // Función para redirigir a la página del visor de PDF con el PDF especificado
-    function redirigirAVisorPDF(pdfSrc) {
-        window.open("visorpdf.html?pdf=" + encodeURIComponent(pdfSrc), "_blank");
-    }
 
     // Agregar eventos a los botones "Visualizar PDF"
     botonesVisualizarPDF.forEach(function(boton) {
         boton.addEventListener("click", function() {
-            // Obtener la ruta del PDF asignada al botón actual
-            var pdfSrc = this.getAttribute('data-src');
-            // Redirigir a la página del visor de PDF con el PDF especificado
-            redirigirAVisorPDF(pdfSrc);
+            // Obtener el nombre del PDF asignado al botón actual
+            var pdfName = this.getAttribute('data-src');
+            // Construir la URL del visorPDF.html con el nombre del PDF
+            var visorUrl = `visorPDF.html?pdf=${pdfName}`;
+            // Abrir el visor del PDF en una nueva pestaña
+            window.open(visorUrl, '_blank');
         });
     });
 });
